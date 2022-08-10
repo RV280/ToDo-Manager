41489e17d6836f2fb69438d9cd91505bcf662758
RV280 <110447513+RV280@users.noreply.github.com>
2022-08-10 13:59:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,67 @@
+"use strict";
 var taskStatus;
 (function (taskStatus) {
-    taskStatus[taskStatus["inProgress"] = 0] = "inProgress";
-    taskStatus[taskStatus["completed"] = 1] = "completed";
+    taskStatus[taskStatus["IN_PROGRESS"] = 0] = "IN_PROGRESS";
+    taskStatus[taskStatus["COMPLETED"] = 1] = "COMPLETED";
 })(taskStatus || (taskStatus = {}));
-var taskIdCounter = 0;
-var taskData = [];
+let taskIdCounter = 0;
+let taskData = [];
 function loadOptions() {
-    var assigneeNames = ["Jayesh", "Shibobrota", "Sarthak", "Anubhav", "Rishab", "Prabhjot", "Rakesh", "Abdul", "Hari Shankar", "Chetan", "Rahul"];
-    var dropDown = document.getElementById("assignee");
-    for (var i = 0; i < assigneeNames.length; i++) {
+    //let assigneeNames = ["Jayesh",  "Shibobrota", "Sarthak", "Anubhav", "Rishab", "Prabhjot", "Rakesh", "Abdul", "Hari Shankar", "Chetan", "Rahul"]
+    let assigneeNames = [{ name: "Rahul", email: "rahul.varshney@dream11.com" },
+        { name: "Jayesh", email: "jayesh@dream11.com" },
+        { name: "Shibobroota", email: "shibobrota@dream11.com" },
+        { name: "Sarthak", email: "sarthak@dream11.com" },
+        { name: "Anubhav", email: "anubhav@dream11.com" },
+        { name: "Rishab", email: "rishab@dream11.com" },
+        { name: "Prabjhot", email: "prabjhot@dream11.com" }];
+    let dropDown = document.getElementById("assignee");
+    for (let i = 0; i < assigneeNames.length; i++) {
         var dropDownOption = document.createElement("option");
-        dropDownOption.textContent = assigneeNames[i];
-        dropDownOption.value = assigneeNames[i];
+        dropDownOption.textContent = assigneeNames[i].name;
+        dropDownOption.value = assigneeNames[i].name;
         if (dropDown) {
             dropDown.appendChild(dropDownOption);
         }
     }
 }
 function insertToDoTask() {
-    var progressTable = document.getElementById("progressTable");
-    var taskValue = document.getElementById("task").value;
-    var assigneeValue = document.getElementById("assignee").value;
-    var dueDate = document.getElementById('dueDate').value;
+    let progressTable = document.getElementById("progressTable");
+    let taskValue = document.getElementById("task").value;
+    let assigneeValue = document.getElementById("assignee").value;
+    let dueDate = document.getElementById('dueDate').value;
     taskIdCounter += 1;
     if (!checkInput(taskValue.trim(), assigneeValue.trim(), dueDate.trim())) {
+        taskIdCounter -= 1;
         return;
     }
-    var todayDate = new Date();
+    let todayDate = new Date();
     todayDate.setHours(0, 0, 0, 0);
     if (dueDate == "") {
         alert("Due Date is not given so end it by EOD");
         dueDate = todayDate.toLocaleDateString();
     }
-    var dueDateObj = new Date(dueDate);
-    var checkbox = document.createElement("input");
+    let dueDateObj = new Date(dueDate);
+    let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = taskIdCounter.toString();
     appendTaskToProgressTable(taskValue, assigneeValue, dueDate, checkbox, progressTable);
     taskData.push({ taskName: taskValue, assignee: assigneeValue,
-        dueDate: dueDateObj, status: taskStatus.inProgress,
+        dueDate: dueDateObj, status: taskStatus.IN_PROGRESS,
         id: taskIdCounter });
     checkbox.addEventListener("click", changeTableOnCheckbox);
     resetInputs();
 }
 function appendTaskToProgressTable(taskValue, assigneeValue, dueDate, checkbox, progressTable) {
-    var row = document.createElement("tr");
-    row.id = "task_id".concat(taskIdCounter);
-    var rowData1 = document.createElement('td');
+    let row = document.createElement("tr");
+    row.id = `task_id${taskIdCounter}`;
+    let rowData1 = document.createElement('td');
     rowData1.appendChild(document.createTextNode(taskValue));
-    var rowData2 = document.createElement('td');
+    let rowData2 = document.createElement('td');
     rowData2.appendChild(document.createTextNode(assigneeValue));
-    var rowData3 = document.createElement('td');
+    let rowData3 = document.createElement('td');
     rowData3.appendChild(document.createTextNode(dueDate));
-    var rowData4 = document.createElement('td');
+    let rowData4 = document.createElement('td');
     rowData4.appendChild(checkbox);
     row.appendChild(rowData1);
     row.appendChild(rowData2);
@@ -64,13 +73,15 @@ function resetInputs() {
     document.getElementById("task").value = "";
     document.getElementById("assignee").value = "";
     document.getElementById("dueDate").value = "";
+    document.getElementsByName("task")[0].placeholder = "Enter a task name";
 }
 function checkInput(taskValue, assigneeValue, dueDate) {
-    var dueDateObj = new Date(dueDate);
-    var todayDate = new Date();
+    let dueDateObj = new Date(dueDate);
+    let todayDate = new Date();
     todayDate.setHours(0, 0, 0, 0);
     if (taskValue == null || taskValue == "") {
-        alert("Task can't be blank");
+        //alert("Task can't be blank");
+        document.getElementsByName("task")[0].placeholder = "Please enter a valid task name!";
         return false;
     }
     else if (assigneeValue == null || assigneeValue == "") {
@@ -84,17 +95,17 @@ function checkInput(taskValue, assigneeValue, dueDate) {
     return true;
 }
 function appendTaskToCompletedTable(cell) {
-    var completedTable = document.getElementById("completedTable");
-    var completedTableRow = document.createElement("tr");
-    var rowData1 = document.createElement('td');
+    let completedTable = document.getElementById("completedTable");
+    let completedTableRow = document.createElement("tr");
+    let rowData1 = document.createElement('td');
     if (cell && cell[0] && cell[0].innerText) {
         rowData1.appendChild(document.createTextNode(cell[0].innerText));
     }
-    var rowData2 = document.createElement('td');
+    let rowData2 = document.createElement('td');
     if (cell && cell[1] && cell[1].innerText) {
         rowData2.appendChild(document.createTextNode(cell[1].innerText));
     }
-    var rowData3 = document.createElement('td');
+    let rowData3 = document.createElement('td');
     if (cell && cell[2] && cell[2].innerText) {
         rowData3.appendChild(document.createTextNode(cell[2].innerText));
     }
@@ -105,12 +116,12 @@ function appendTaskToCompletedTable(cell) {
 }
 function changeTableOnCheckbox(event) {
     var _a;
-    var taskId = +event.currentTarget.id;
+    let taskId = +event.currentTarget.id;
     console.log(taskId);
-    taskData[taskId - 1].status = taskStatus.completed;
-    var progressTableRow = document.getElementById("task_id".concat(taskId));
-    (_a = document.getElementById("task_id".concat(taskId))) === null || _a === void 0 ? void 0 : _a.remove();
-    var cell = progressTableRow === null || progressTableRow === void 0 ? void 0 : progressTableRow.childNodes;
+    taskData[taskId - 1].status = taskStatus.COMPLETED;
+    let progressTableRow = document.getElementById(`task_id${taskId}`);
+    (_a = document.getElementById(`task_id${taskId}`)) === null || _a === void 0 ? void 0 : _a.remove();
+    let cell = progressTableRow === null || progressTableRow === void 0 ? void 0 : progressTableRow.childNodes;
     if (cell) {
         appendTaskToCompletedTable(cell);
     }
